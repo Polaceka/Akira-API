b2f63317e33346864f9aad71200bfd3f1a0e9f05
Polaceka <ad.polacek@gmail.com>
2020-10-20 23:57:13 +0200

    - Renamed /tracks -> /track - Created Placeholder route

--- main.go
+++ main.go
@@ -18,10 +18,15 @@ func main() {
 	v1 := r.Group("/v1")
 	v1.Use(authRequired)
 	{
-		v1.GET("/tracks", handleGetTracks)
-		v1.GET("tracks/:name", handlerGetOneTracks)
-		v1.POST("/tracks", handleCreateTrack)
+		v1.GET("/track", handleGetTracks)
+		v1.GET("track/:name", handlerGetOneTracks)
+		v1.POST("/track", handleCreateTrack)
 
+		/* placeholder
+		v1.GET("/event", handleGetEvents)
+		v1.GET("/event/:name", handleGetOneEvent)
+		v1.Post("/event")
+		*/
 	}
 
 	// currently for dev ussage
